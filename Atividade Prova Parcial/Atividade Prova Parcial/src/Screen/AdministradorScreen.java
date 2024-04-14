8a7369c27804b4e0b7c4d4df18ee705154b78d76
jaourzedo <jaourzedo@gmail.com>
2024-04-13 23:21:40 -0300

    Adicionado a AdministradorScreen.

--- Atividade Prova Parcial/Atividade Prova Parcial/src/Screen/AdministradorScreen.java
+++ Atividade Prova Parcial/Atividade Prova Parcial/src/Screen/AdministradorScreen.java
@@ -1,4 +1,38 @@
 package Screen;
 
-public class AdministradorScreen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdministradorScreen extends JFrame {
+
+    public AdministradorScreen() {
+        super("Tela do Administrador");
+        setSize(400, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JButton btnGestaoLivros = new JButton("Gestão de Livros");
+        JButton btnCadastrarUsuario = new JButton("Cadastrar Usuário");
+
+        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+        add(btnGestaoLivros);
+        add(btnCadastrarUsuario);
+
+        btnGestaoLivros.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Adicione aqui o código para abrir a tela de gestão de livros
+            }
+        });
+
+        btnCadastrarUsuario.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Adicione aqui o código para abrir a tela de cadastro de usuário
+            }
+        });
+
+        setVisible(true);
+    }
 }
